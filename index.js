5e3a1d82fa797e6160b6513615150f009fb2c2bc
Jonathan Cormier <jonathan@cormier.co>
2023-07-13 22:18:40 -0300

    Formatting and commands mem & wiretiger
    Clean up indentation of code.
    Add command mem to output memory (virtual and physical)
    Add command wiredtiger for output of WiredTiger cache, etc.

--- index.js
+++ index.js
@@ -25,56 +25,56 @@ function _loadFunctions(obj, ns) {
 let mpModule = (function() {
     let version = "0.1";
 
-    let hostInfo = db.hostInfo();
-    let serverBuildInfo = db.serverBuildInfo();
-    let serverStatus = db.serverStatus();
+    let host_info = db.hostInfo();
+    let server_build_info = db.serverBuildInfo();
+    let server_status = db.serverStatus();
     
     function arch() {
         return db.serverBits();
     }
 
     function uname() {
-        let systemHostname = hostInfo['system']['hostname'];
-        let osType = hostInfo['os']['type'];
-        let extraVersion = hostInfo['extra']['versionString'];
+        let system_hostname = host_info['system']['hostname'];
+        let os_type = host_info['os']['type'];
+        let extra_version = host_info['extra']['versionString'];
     
         let values = [
-            osType,
-            systemHostname,
-            extraVersion];
+            os_type,
+            system_hostname,
+            extra_version];
     
         return values.join(" ");
     }
 
     function os() {
-        let osName = hostInfo['os']['name'];
-        let osType = hostInfo['os']['type'];
-        let osVersion = hostInfo['os']['version'];
+        let os_name = host_info['os']['name'];
+        let os_type = host_info['os']['type'];
+        let os_version = host_info['os']['version'];
     
         let values = [
-            osName,
-            osType,
-            osVersion
+            os_name,
+            os_type,
+            os_version
         ];
     
         return values.join(" ");
     }
     
     function time() {
-        let isoDate = hostInfo['system']['currentTime']
-        let humanReadableDate = isoDate.toString();
+        let iso_date = host_info['system']['currentTime']
+        let human_readable_date = iso_date.toString();
         
-        return humanReadableDate;
+        return human_readable_date;
     }
 
     function mname() {
-        let mongoVersion = serverBuildInfo['version'];
-        let mongoGitVersion = serverBuildInfo['gitVersion'];
+        let mongo_version = server_build_info['version'];
+        let mongo_git_version = server_build_info['gitVersion'];
 
         let values = [
             "MongoDB",
-            mongoVersion,
-            mongoGitVersion
+            mongo_version,
+            mongo_git_version
         ];
 
         return values
@@ -83,41 +83,41 @@ let mpModule = (function() {
 
     function uptime() {
         // Uptime
-        var uptimeSeconds = serverStatus.uptime;
-        var uptimeDays = Math.floor(uptimeSeconds / (24 * 60 * 60));
-        var uptimeHours = Math.floor((uptimeSeconds % (24 * 60 * 60)) / (60 * 60));
-        var uptimeMinutes = Math.floor((uptimeSeconds % (60 * 60)) / 60);
+        let uptime_seconds = server_status.uptime;
+        let uptime_days = Math.floor(uptime_seconds / (24 * 60 * 60));
+        let uptime_hours = Math.floor((uptime_seconds % (24 * 60 * 60)) / (60 * 60));
+        let uptime_minutes = Math.floor((uptime_seconds % (60 * 60)) / 60);
     
         // Current time
-        var currentDateTime = new Date();
-        var formattedTime = currentDateTime.getHours().toString().padStart(2, '0') + ':' + currentDateTime.getMinutes().toString().padStart(2, '0');
+        let current_date_time = new Date();
+        let formatted_time = current_date_time.getHours().toString().padStart(2, '0') + ':' + current_date_time.getMinutes().toString().padStart(2, '0');
         
         // Current queue & active clients
-        var activeClients = serverStatus.globalLock.activeClients;
-        var currentQueue = serverStatus.globalLock.currentQueue;
+        let active_clients = server_status.globalLock.activeClients;
+        let current_queue = server_status.globalLock.currentQueue;
       
-        let activeClientsValues = [
-            activeClients['total'],
-            activeClients['readers'],
-            activeClients['writers']
+        let active_clients_values = [
+            active_clients['total'],
+            active_clients['readers'],
+            active_clients['writers']
         ];
         
         let currentQueueValues = [
-            currentQueue['total'],
-            currentQueue['readers'],
-            currentQueue['writers']
+            current_queue['total'],
+            current_queue['readers'],
+            current_queue['writers']
         ];
     
         let values = [
-            formattedTime,
+            formatted_time,
             '  up ',
-            uptimeDays,
+            uptime_days,
             ' days, ',
-            uptimeHours,
+            uptime_hours,
             ':',
-            uptimeMinutes.toString().padStart(2, '0'),
+            uptime_minutes.toString().padStart(2, '0'),
             ', clients: ',
-            activeClientsValues.join("/"),
+            active_clients_values.join("/"),
             ', queue: ',
             currentQueueValues.join("/")
         ];
@@ -127,28 +127,69 @@ let mpModule = (function() {
 
     // MongoDB Performance Tuning, page 212
     function txn_counts() {
-	var ssTxns = db.serverStatus().transactions;
+        let ss_txns = db.serverStatus().transactions;
+    
+        print(ss_txns.totalStarted + 0, 'transactions started');
+        print(ss_txns.totalAborted + 0, 'transactions aborted');
+        print(ss_txns.totalCommitted + 0, 'transactions committed');
 
-	print(ssTxns.totalStarted + 0, 'transactions started');
-	print(ssTxns.totalAborted + 0, 'transactions aborted');
-	print(ssTxns.totalCommitted + 0, 'transactions committed');
+        var percentage_aborted = Math.round(ss_txns.totalAborted * 100 / ss_txns.totalStarted);
+        if (ss_txns.totalStarted == 0) percentage_aborted = 0;
 
-	var percentageAborted = Math.round(ssTxns.totalAborted * 100 / ssTxns.totalStarted);
-	if (ssTxns.totalStarted == 0) percentageAborted = 0;
-
-	return percentageAborted + "% txns aborted";
+        return percentage_aborted + "% txns aborted";
     }
 
     function ping() {
-	let startTime = new Date();
+	    let start_time = new Date();
         let pong = db.runCommand("ping");
-	let endTime = new Date();
-	let timeDiff = endTime - startTime;
-	let ok = pong.ok;
-	
-	return "P0NG! " + timeDiff + "ms, ok: " + ok;
+	    let end_time = new Date();
+        let time_diff = end_time - start_time;
+        let ok = pong.ok;
+        let ping_msg = "P0NG! " + time_diff + "ms, ok: " + ok;
+
+        if (timeDiff >= 100) ping_msg += ", high latency";
+
+        return ping_msg;
     }
 
+    function mem() {
+    	let server_stats = db.serverStatus();
+        let wired_tiger_cache_size = Math.round(server_stats.wiredTiger.cache['bytes currently in the cache'] / 1048576);
+
+        mem_msg = `Mongod virtual memory:  ${serverStats.mem.virtual}MB\n`;
+        mem_msg += `Mongod resident memory: ${serverStats.mem.resident}MB\n`;
+        mem_msg += `WiredTiger cache size:  ${wired_tiger_cache_size}MB`;
+
+        return mem_msg;
+    }
+
+    function wiredtiger() {
+        let wt = db.serverStatus().wiredTiger;
+	    let wt_cache_size = Math.round(wt.cache['bytes currently in the cache'] / 1048576);
+
+        // Cache reads
+	    let wt_cache_disk_reads = wt.cache['application threads page read from disk to cache count'];
+        let wt_cache_disk_read_time = wt.cache['application threads page read from disk to cache time (usecs)'];
+        let wt_cache_avg_reads = wt_cache_disk_read_time/1000/wt_cache_disk_reads;
+	    
+        // Cache writes
+        let wt_cache_disk_writes = wt.cache['application threads page write from cache to disk count'];
+        let wt_cache_disk_write_time = wt.cache['application threads page write from cache to disk time (usecs)'];
+        let wt_cache_avg_writes = wt_cache_disk_write_time/1000/wt_cache_disk_writes;
+
+        // Build WiredTiger command output
+        wt_msg = `WiredTiger cache size: ${wt_cache_size}MB\n`;
+        wt_msg += `WiredTiger cache disk read count:  ${wt_cache_disk_reads}\n`
+        wt_msg += `WiredTiger cache disk write count: ${wt_cache_disk_writes}\n`
+        wt_msg += `WiredTiger cache disk read time:  ${wt_cache_disk_read_time}ms\n`
+        wt_msg += `WiredTiger cache disk write time: ${wt_cache_disk_write_time}ms\n`
+
+        wt_msg += `Avg. disk read time:  ${wt_cache_avg_reads}ms`;
+        wt_msg += `Avg. disk write time: ${wt_cache_avg_writes}ms\n`;
+
+	    return wt_msg;
+    }
+    
     // Expose the public functions
     return {
         version: version,
@@ -158,8 +199,10 @@ let mpModule = (function() {
         time: time,
         mname: mname,
         uptime: uptime,
-	txn_counts: txn_counts,
-        ping: ping
+	    txn_counts: txn_counts,
+        ping: ping,
+	    mem: mem,
+	    wiredtiger, wiredtiger
     };
 })();
 
